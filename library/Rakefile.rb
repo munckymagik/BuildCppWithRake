36eca04f7e6bf5d2edd33e3439ffe190925a5fe5
Dan <daniel@feynman.(none)>
2013-05-13 21:40:25 +0100

    Finish the Rakefile for the library

--- library/Rakefile.rb
+++ library/Rakefile.rb
@@ -1,11 +1,32 @@
-CLEAN = FileList['obj/*.o']
-CLOBBER = FileList['bin/*']
+require 'rake/clean'
 
+objdir = 'obj'
+bindir = 'bin'
 
-task :default => ['obj/mylib.o']
+directory objdir
+directory bindir
 
+SRC = FileList['src/*.cpp']
+OBJ = FileList.new() #SRC.pathmap("%{^src,obj}X.o")
 
-file 'obj/mylib.o' => ['src/mylib.cpp'] do
-	sh "g++ -c -o obj/mylib.o -Iinclude src/mylib.cpp"
+task :default => ['bin/libmylib.a']
+
+SRC.each do |fn|
+	obj = fn.pathmap("%{^src,obj}X.o")
+	OBJ.include(obj)
+	file obj do
+		sh "g++ -c -o #{obj} -Iinclude #{fn}"
+	end
+
+	file obj => objdir
 end
+
+file 'bin/libmylib.a' => [OBJ, bindir] do |t|
+	sh "ar rcs #{t.name} #{OBJ}"
+end
+
+file 'obj/mylib.o' => ['include/mylib.h', 'src/mylib.cpp']
+
+CLEAN.include(OBJ)
+CLOBBER.include([objdir, bindir])
 
